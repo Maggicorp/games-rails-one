42b5428073cd7058d5e060eae6c4172e17715744
Maggicorp <mvjcasey@gmail.com>
2017-08-24 15:02:37 -0400

    Adds game and teams
    with models and serializers, only includes get index method for games

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170824182230) do
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.string   "player_names"
+    t.integer  "score"
+    t.integer  "game_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["game_id"], name: "index_teams_on_game_id"
+  end
 
 end
